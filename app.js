6f814585d4a6168d09dabb86e13c5efaefed5538
arivaatral780 <67435089+arivaatral780@users.noreply.github.com>
2022-07-19 23:50:29 +0530

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-var express=require('express');
-var app=express();
-app.use(express.urlencoded({extended:false}));
-app.set("view engine","ejs");
-app.use('/',require('./routes/registration-route'));
-app.use('/dashboard',require('./routes/dashboard-route'));
-app.use("/login",require("./routes/login-route"));
-console.log("The server is running at http://localhost:8080");
-app.listen(8080);
