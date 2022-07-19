afc4814738260f8183b510e409724a19d1195ca6
arivaatral780 <67435089+arivaatral780@users.noreply.github.com>
2022-07-20 00:06:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var express=require('express');
+var app=express();
+app.use(express.urlencoded({extended:false}));
+app.set("view engine","ejs");
+app.use('/',require('./routes/registration-route'));
+app.use('/dashboard',require('./routes/dashboard-route'));
+app.use("/login",require("./routes/login-route"));
+app.use("/logout",require("./routes/logout-route"));
+console.log("The server is running at http://localhost:8080");
+app.listen(8080);
